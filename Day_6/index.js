1c8ded5230a9c1d3c32c1c30d3b796e5f0f7d4d4
Abhi Changela <abhichangela@gmail.com>
2025-10-21 17:45:46 +0530

    Explain package.json

---
+++ Day_6/index.js
@@ -0,0 +1,18 @@
+/* To add package.json run command: npm init
+
+After running npm init it will ask few things like:
+
+package name: Add package name here commanly we add project name here
+version: which version of our project is
+description: description of our project
+entry point: first file we want to run
+test command: testing command that we want to add in our project
+git repository: any git repo on which we are uploading this project
+keywords: any keywords of our project
+author: author of this project
+license: any license we want to use in this project
+type: commonjs or typescript 
+
+Now we can install any private or public package into our project & use that in our project.
+Package.json file most important file of our project like its a heart of project if we lost 
+package.json file then we have lost our project so be careful with package.json file. */
